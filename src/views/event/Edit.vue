1e769dbf7fbec03bbaadb13e290afb4201dc8fc1
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-07-29 01:01:39 +0700

    1.6 Add a new edit button component
    The message should state that the data has been updated

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,28 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+    inject: ["GStore"], //<---- Inject the glebal Store
+  methods: {
+    edit() {
+      // Assuming successful API call to register them
+      // Set a flash massage to appear in the next page loaded which says
+      // 'You are successfully registered for ' + this.event.title
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: {id: this.event.id}
+      }),
+      this.GStore.flashMessage = 'You are successfully updated for ' + this.event.title
+      setTimeout(() => {
+        // After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+    }
+  }
 }
 </script>
