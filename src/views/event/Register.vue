fdfaa39862a5c575854a266620d7464e2b27e274
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-07-29 00:54:36 +0700

    1.5 Flash Message to register page
    when click button Register Me

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,29 @@
 <template>
   <p>Regstration form here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ["GStore"], //<---- Inject the glebal Store
+  methods: {
+    register() {
+      // Assuming successful API call to register them
+      // Set a flash massage to appear in the next page loaded which says
+      // 'You are successfully registered for ' + this.event.title
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: {id: this.event.id}
+      }),
+      this.GStore.flashMessage = 'You are successfully registered for ' + this.event.title
+      setTimeout(() => {
+        // After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+    }
+  }
+  
 }
 </script>
